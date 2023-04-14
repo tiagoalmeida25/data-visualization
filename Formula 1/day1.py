964452e59ad995eeb261d5f4db4d37fb0d07af51
Tiago Almeida <62519958+tiagoalmeida25@users.noreply.github.com>
2023-04-14 23:51:51 +0100

    karts analysis

--- Formula 1/day1.py
+++ Formula 1/day1.py
@@ -8,41 +8,62 @@ from datetime import timedelta
 
 fastf1.Cache.enable_cache('C:/Users/tiago/Desktop/Projetos/Python/data-visualization/Formula 1/Cache')
 
-event = fastf1.get_event(2021, 22)
+YEAR = 2022
+RACE = 'Bahrain'
+# ROUND = 1
+SESSION = 'R'
+
+event = fastf1.get_event(YEAR, RACE)
 
 print(event.EventName)
 
-session = event.get_session('R')
+session = event.get_session(SESSION)
+
 session.load()
-print(session.laps.columns)
-print(session.laps)
-race = []
-for driver, lap_time, lap_number in zip(session.laps.Driver, session.laps.LapTime, session.laps.LapNumber):
-    race.append({'driver':driver,
-                'lap-time':lap_time,
-                'lap-number':lap_number})
+
+laps = session.laps
+
+alo_laps = laps.pick_driver('ALO')
+print(alo_laps)
+
+alo_fast = alo_laps.pick_fastest()
+print(alo_fast)
+
+alo_car = alo_fast.get_car_data()
+print(alo_car)
+
+
+
+
+
+# race = []
+# for driver, lap_time, lap_number in zip(session.laps.Driver, session.laps.LapTime, session.laps.LapNumber):
+#     race.append({'driver':driver,
+#                 'lap-time':lap_time,
+#                 'lap-number':lap_number})
     
 
-df = pd.DataFrame(race)
+# df = pd.DataFrame(race)
 
-print(df.head())
-
-
-data = [go.Scatter(x=df[df['driver']=='VER']['lap-number'],
-                   y=pd.to_timedelta(df[df['driver']=='VER']['lap-time']),
-                #    text = [str(timedelta(seconds=df[df['driver']=='VER']['lap-time']))]
-                   )]
+# print(df.head())
 
 
 
-layout = go.Layout(title = 'F1 time per lap',
-                   yaxis=dict(
-                       tickmode='array',
-                       tickvals=[60000000000,88000000000,90000000000,92000000000,105000000000],
-                       ticktext=['1:00:00', '1:28:00','1:30:00', '1:32:00','1:45:00']
-                   ))
+# data = [go.Scatter(x=df[df['driver']=='VER']['lap-number'],
+#                    y=pd.to_timedelta(df[df['driver']=='VER']['lap-time']),
+#                 #    text = [str(timedelta(seconds=df[df['driver']=='VER']['lap-time']))]
+#                    )]
 
-# fig.update_layout(layout)
 
-fig = go.Figure(data, layout)
-pyo.plot(fig)
+
+# layout = go.Layout(title = 'F1 time per lap',
+#                    yaxis=dict(
+#                        tickmode='array',
+#                        tickvals=[60000000000,88000000000,90000000000,92000000000,105000000000],
+#                        ticktext=['1:00:00', '1:28:00','1:30:00', '1:32:00','1:45:00']
+#                    ))
+
+# # fig.update_layout(layout)
+
+# fig = go.Figure(data, layout)
+# pyo.plot(fig)
